82ddcfd42bce5ce41ffc7c88ceaae7a542954637
Natalia Lapina <lapinann08@gmail.com>
2024-08-09 16:12:14 +0200

    fix formatting

--- api/src/routers/meals.js
+++ api/src/routers/meals.js
@@ -12,15 +12,10 @@ import {
 const router = express.Router();
 
 router.get("/", getApiInfo);
-
 router.get("/future-meals", getFutureMeals);
-
 router.get("/past-meals", getPastMeals);
-
 router.get("/all-meals", getAllMeals);
-
 router.get("/first-meal", getFirstMeal);
-
 router.get("/last-meal", getLastMeal);
 
 export default router;

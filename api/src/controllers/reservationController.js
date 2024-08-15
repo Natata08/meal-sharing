22ce091e0c7a2e0b63d0c2165f2e8d01a54a662f
Natalia Lapina <lapinann08@gmail.com>
2024-08-15 16:33:11 +0200

    add check of available reservation spots

--- api/src/controllers/reservationController.js
+++ api/src/controllers/reservationController.js
@@ -12,10 +12,30 @@ export const getAllReservations = async (req, res) => {
 
 export const addNewReservation = async (req, res) => {
   try {
+    //checking available spots for reservation
+    const { meal_id, number_of_guests } = req.body;
+    const meal = await knex("meal").where({ id: meal_id }).first();
+    if (!meal) {
+      return res.status(404).json({ error: "There is no meal with such ID" });
+    }
+    const query = await knex("reservation")
+      .where({ meal_id: meal_id })
+      .sum("number_of_guests")
+      .first();
+
+    const currentReservations = parseInt(query["sum(`number_of_guests`)"]) || 0;
+    const availableSpots = meal.max_reservations - currentReservations;
+    if (number_of_guests > availableSpots) {
+      return res.status(400).json({
+        error: "Not enough available spots",
+        "available spots": availableSpots,
+      });
+    }
+
     const [id] = await knex("reservation").insert(req.body);
     const [newReservation] = await knex("reservation").where({ id: id });
     res.status(201).json({
-      message: "New meal added successfully",
+      message: "New reservation added successfully",
       reservation: newReservation,
     });
   } catch (error) {

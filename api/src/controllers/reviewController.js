24014021a00e73cd3804eb5b3494f14cdec3fb2c
Natalia Lapina <lapinann08@gmail.com>
2024-08-21 15:25:14 +0200

    add GET and POST request for /reviews

--- api/src/controllers/reviewController.js
+++ api/src/controllers/reviewController.js
@@ -1,8 +1,28 @@
 import knex from "../database_client.js";
 
-export const getAllReviews = async () => {};
+export const getAllReviews = async (req, res) => {
+  try {
+    const reviews = await knex.from("review").select();
+    res.json(reviews);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error fetching reviews" });
+  }
+};
 
-export const addNewReview = async () => {};
-export const getReviewById = async () => {};
-export const updateReviewById = async () => {};
-export const deleteReviewById = async () => {};
+export const addNewReview = async (req, res) => {
+  try {
+    const [id] = await knex("review").insert(req.body);
+    const newReview = await knex("review").where({ id: id }).first();
+    res
+      .status(201)
+      .json({ message: "New review added successfully", review: newReview });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error adding new review" });
+  }
+};
+
+export const getReviewById = async (req, res) => {};
+export const updateReviewById = async (req, res) => {};
+export const deleteReviewById = async (req, res) => {};

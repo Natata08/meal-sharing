3e8f89e1dfccfbc50e42b529fe5d3ebf89b60e1a
Natalia Lapina <lapinann08@gmail.com>
2024-09-28 14:22:38 +0200

    create SocialMediaIcons component

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,7 +1,9 @@
 "use client";
 
-import { Box, Container, Typography, IconButton, Link } from "@mui/material";
-import { Facebook, Twitter, Instagram, LinkedIn } from "@mui/icons-material";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import SocialMediaIcons from "@/components/SocialMediaIcons";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
@@ -21,46 +23,9 @@ export default function Footer() {
     >
       <Container maxWidth='sm'>
         <Typography variant='body1' align='center'>
-          © {currentYear} Your Company Name. All rights reserved.
+          © {currentYear} Meal Sharing. All rights reserved.
         </Typography>
-        <Box sx={{ mt: 2, display: "flex", justifyContent: "center" }}>
-          <IconButton
-            component={Link}
-            href='https://facebook.com/yourcompany'
-            target='_blank'
-            rel='noopener noreferrer'
-            color='inherit'
-          >
-            <Facebook />
-          </IconButton>
-          <IconButton
-            component={Link}
-            href='https://twitter.com/yourcompany'
-            target='_blank'
-            rel='noopener noreferrer'
-            color='inherit'
-          >
-            <Twitter />
-          </IconButton>
-          <IconButton
-            component={Link}
-            href='https://instagram.com/yourcompany'
-            target='_blank'
-            rel='noopener noreferrer'
-            color='inherit'
-          >
-            <Instagram />
-          </IconButton>
-          <IconButton
-            component={Link}
-            href='https://linkedin.com/company/yourcompany'
-            target='_blank'
-            rel='noopener noreferrer'
-            color='inherit'
-          >
-            <LinkedIn />
-          </IconButton>
-        </Box>
+        <SocialMediaIcons />
       </Container>
     </Box>
   );

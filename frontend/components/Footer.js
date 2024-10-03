9a5933db4a9510087bbd681a3202c570fab27c7e
Natalia Lapina <lapinann08@gmail.com>
2024-10-03 15:01:40 +0200

    change Footer

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -4,26 +4,28 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import SocialMediaIcons from "@/components/SocialMediaIcons";
+import { usePathname } from "next/navigation";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
 
   return (
     <Box
       component='footer'
+      className={isHomePage ? styles.hidden : undefined}
       sx={{
         py: 3,
         px: 2,
         mt: "auto",
-        backgroundColor: (theme) =>
-          theme.palette.mode === "light"
-            ? theme.palette.grey[200]
-            : theme.palette.grey[800],
+        bgcolor: "rgb(111, 156, 61, 0.5)",
       }}
     >
       <Container maxWidth='lg'>
         <Typography variant='body1' align='center'>
-          © {currentYear} Meal Sharing. All rights reserved.
+          © {currentYear} FoodFriends. All rights reserved.
         </Typography>
         <SocialMediaIcons />
       </Container>

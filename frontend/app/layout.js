450d8d1e6972e32dda62381dbf6e703ecf38fa3d
Natalia Lapina <lapinann08@gmail.com>
2024-10-03 14:56:52 +0200

    change colors in theme

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,42 +1,32 @@
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 
-export const metadata = {
-  title: "Meal-sharing app",
-  description: "App for people who share their meal",
-};
+import theme from "./theme";
+import { ThemeProvider } from "@mui/system";
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            minHeight: "100vh",
-          }}
-        >
-          <CssBaseline />
-          <Navbar />
-          <Container
-            component='main'
-            maxWidth='lg'
+      <body suppressHydrationWarning={true}>
+        <ThemeProvider theme={theme}>
+          <Box
             sx={{
-              flexGrow: 1,
               display: "flex",
               flexDirection: "column",
-              py: 3, // Add some padding to the top and bottom
+              minHeight: "100vh",
             }}
           >
+            <CssBaseline />
+            <Header />
+
             {children}
-          </Container>
-          <Footer />
-        </Box>
+
+            <Footer />
+          </Box>
+        </ThemeProvider>
       </body>
     </html>
   );

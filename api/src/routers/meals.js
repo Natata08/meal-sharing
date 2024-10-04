5ec2db47c25e6ded4a67475b8407bdc76a8d6af4
Natalia Lapina <lapinann08@gmail.com>
2024-10-04 14:39:20 +0200

    add endpoint for getting summary of meals

--- api/src/routers/meals.js
+++ api/src/routers/meals.js
@@ -16,6 +16,8 @@ import {
   getFirstMeal,
   getLastMeal,
   getReviewsForMeal,
+  getMealsSummary,
+  getReservationsForMeal,
 } from "../controllers/mealController.js";
 
 const router = express.Router();
@@ -26,10 +28,13 @@ router.get("/first-meal", getFirstMeal);
 router.get("/last-meal", getLastMeal);
 
 router.get("/", getMealsWithQueries);
+router.get("/summary", getMealsSummary);
 router.post("/", validateRequest(mealAddSchema), addNewMeal);
 router.get("/:id", getMealById);
 router.get("/:meal_id/reviews", getReviewsForMeal);
+router.get("/:meal_id/reservations", getReservationsForMeal);
 router.put("/:id", validateRequest(mealUpdateSchema), updateMealById);
 router.delete("/:id", deleteMealById);
+router.get("/summary", getMealsSummary);
 
 export default router;

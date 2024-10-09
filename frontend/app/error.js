08f34590db31c1414a2825b5d8646e8df53c3b42
Natalia Lapina <lapinann08@gmail.com>
2024-10-09 16:34:10 +0200

    change Error component

--- frontend/app/error.js
+++ frontend/app/error.js
@@ -2,8 +2,13 @@
 import Container from "@mui/material/Container";
 import Alert from "@mui/material/Alert";
 import Button from "@mui/material/Button";
+import { useEffect } from "react";
 
 export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
   return (
     <Container maxWidth='md' sx={{ mt: 13 }}>
       <Alert
@@ -19,7 +24,7 @@ export default function Error({ error, reset }) {
           </Button>
         }
       >
-        {error.message || "We're having trouble loading the meals right now."}
+        {"We're having trouble loading the meals right now."}
       </Alert>
     </Container>
   );
